7964ad6668ff5ea956e9de1b7a8724093f631823
nero <harshtgi00@gmail.com>
2022-09-26 20:24:37 -0400

    Plotting file is working.

--- plot.py
+++ plot.py
@@ -11,10 +11,10 @@ now = datetime.now()
 dtime = now.strftime('%m%d')
 
 plt.rcParams.update(plt.rcParamsDefault)
-plt.style.use('seaborn-v0_8-whitegrid')
+plt.style.use('seaborn-whitegrid')
 plt.rcParams['font.family'] = 'serif'
 
-df = pd.read_excel('./results/0923/2022-0923_resnet34_malmotest.xlsx')
+df = pd.read_excel('./results/0923_chris.xlsx')
 df = df[df['status'] == 'complete']
 df = df[df['mod'] == 1]
 df = df.iloc[:, 5:-1]
@@ -23,7 +23,6 @@ df.obsInc = df.obsInc.apply(literal_eval)
 df.accInc = df.accInc.apply(literal_eval)
 df.runTimeInc = df.runTimeInc.apply(literal_eval)
 df.runDistInc = df.runDistInc.apply(literal_eval)
-# df.trainTimeInc = df.trainTimeInc.apply(literal_eval)
 
 dList = list(set(df['data']))
 pList = list(set(df['learner']))
@@ -70,29 +69,12 @@ for d in dList:
                 avg_tRun.append(avg)
                 std_tRun.append(std)
 
-                # x = np.array(list(temp.trainTimeInc))
-                # avg = np.mean(x, axis = 0)/60
-                # std = np.std(x, axis = 0)/60
-                # avg_tTrain.append(avg)
-                # std_tTrain.append(std)
-
-                # x = np.array(list(temp.trainTimeInc))
-                # x = np.array([[ix[j+1] - ix[j] for j in range(len(ix)-1)] for ix in x])
-                # avg = np.mean(x, axis = 0)/60
-                # std = np.std(x, axis = 0)/60
-                # avg_tTrainIt.append(avg)
-                # std_tTrainIt.append(std)
-
                 x = [i for i in range(len(avg))]
                 x0.append(x)
 
                 if b == 'random':           b = 'uniform'
                 if b == 'classWt':          b = 'low class weight'
                 if b == 'clusterWt':        b = 'low cluster weight'
-                if b == 'SVM_redistrict':   b = 'redistrict'
-                if b == 'SVM_uniform':      b = 'uniform'
-                if b == 'clusterStdLow':    b = 'low cluster variance'
-                if b == 'clusterStdHigh':   b = 'high cluster variance'
 
                 label = p + ' ' + b
                 labels.append(label)
@@ -106,7 +88,7 @@ del x, avg, std, label, d, dList, p, pList, b, bList, temp
 # fig, ((ax1,ax2), (ax3,ax4)) = plt.subplots(2, 2, sharex = 'col', sharey = 'row', figsize=(10,7))
 fig, (ax1, ax2) = plt.subplots(1, 2, sharex='col', sharey='row', figsize=(10, 5))
 plt.rcParams.update(plt.rcParamsDefault)
-plt.style.use('seaborn-v08-whitegrid')
+plt.style.use('seaborn-whitegrid')
 plt.rcParams['font.family'] = 'serif'
 
 
@@ -141,36 +123,20 @@ def get_props(name, d):
     aqua = sns.color_palette()[9]
     black = 'black'
 
-    if name.startswith('CBCL'): name = 'CBCL-' + label[4:]
+    name = 'CBCL-' + name
 
-    if name == 'SVM redistrict':
-        ans = [black, ':', 'x']
-    elif name == 'SVM uniform':
-        ans = [orange, '-', '^']
-
-    elif name == 'CBCL-SVM low class weight' and d == 'cifar':
+    if name == 'CBCL-SVM low class weight':
         ans = [blue, '-.', 's']
-    elif name == 'CBCL-SVM uniform' and d == 'cifar':
+    elif name == 'CBCL-SVM uniform':
         ans = [green, '--', 'o']
-    elif name == 'CBCL-SVM low cluster weight' and d == 'cifar':
+    elif name == 'CBCL-SVM low cluster weight':
         ans = [purple, ':', '^']
-    elif name == 'CBCL-SVM low cluster variance' and d == 'cifar':
-        ans = [grey, '-', 'o']
-    elif name == 'CBCL-SVM high cluster variance' and d == 'cifar':
-        ans = [brown, '--', 's']
-
-
-    elif name == 'CBCL-PR low class weight' and d == 'grocery':
-        ans = [blue, '-.', 's']
-    elif name == 'CBCL-PR uniform' and d == 'grocery':
-        ans = [green, '--', 'o']
-    elif name == 'CBCL-PR low cluster weight' and d == 'grocery':
-        ans = [purple, ':', '^']
-    elif name == 'CBCL-PR low cluster variance' and d == 'grocery':
-        ans = [grey, '-', 'o']
-    elif name == 'CBCL-PR high cluster variance' and d == 'grocery':
-        ans = [brown, '--', 's']
-
+    elif name == 'CBCL-WVS low class weight':
+        ans = [orange, '-.', 's']
+    elif name == 'CBCL-WVS uniform':
+        ans = [red, '--', 'o']
+    elif name == 'CBCL-WVS low cluster weight':
+        ans = [grey, ':', '^']
     else:
         ans = []
 
@@ -183,8 +149,6 @@ def get_props(name, d):
             ans.append(6)
         elif ans[2] == '^':
             ans.append(6)
-
-    if name.startswith('CBCL-SVM'): name = 'CBCL-PR' + name[8:]
 
     return ans, name
 
@@ -229,41 +193,25 @@ H2new = [H2[i] for i in ix2]
 L1new = [L1[i] for i in ix1]
 L2new = [L2[i] for i in ix2]
 
-# ax3.xaxis.get_major_ticks()[0].label1.set_visible(False)
-ax2.xaxis.get_major_ticks()[0].label1.set_visible(False)
-
 ax1.yaxis.get_major_ticks()[0].label1.set_visible(False)
-# ax3.yaxis.get_major_ticks()[0].label1.set_visible(False)
-
-
+ax2.xaxis.get_major_ticks()[0].label1.set_visible(False)
 ax1.legend(H1new, L1new, loc=2, prop={'size': 9})
 ax2.legend(H2new, L2new, loc=4, prop={'size': 9})
-# ax3.legend(loc=2, prop={'size': 9})
-# ax4.legend(loc=2, prop={'size': 9})
 
 ax1.set_title('CIFAR-100 (100 random initial images) ', fontweight='bold', size=9)
 ax2.set_title('GROCERY STORE (81 random initial images)', fontweight='bold', size=9)
 ax1.set_ylabel('Test Prediction Accuracy (%)', fontweight='bold', size=9)
-# ax3.set_ylabel('Train Time (min)', fontweight = 'bold')
 
 ax1.set_xlabel('Search Time (min)', fontweight='bold', size=9)
 ax2.set_xlabel('Search Time (min)', fontweight='bold', size=9)
 
-ax1.set_ylim([15, 85])
-# ax3.set_ylim([0,15])
-
+ax1.set_ylim([0, 50])
 ax1.set_xlim([0, 125])
 ax2.set_xlim([0, 62.5])
-# ax3.set_ylim([0,0.45])
 
 fig.align_ylabels()
 plt.subplots_adjust(wspace=0.025, hspace=0.05)
 
-# ax1.set_rasterization_zorder(0)
-# ax2.set_rasterization_zorder(0)
-# ax3.set_rasterization_zorder(0)
-# ax4.set_rasterization_zorder(0)
-# plt.savefig('e2_best_0615.eps', format='eps', bbox_inches='tight')
-plt.savefig('./img/{}_malmorun.png'.format(dtime), bbox_inches='tight')
+plt.savefig('./results/figure/{}_robothor.png'.format(dtime), bbox_inches='tight')
 
 plt.show()
